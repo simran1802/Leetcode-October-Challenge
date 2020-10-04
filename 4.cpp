d2fc24dfda6a0f7a3cac428190b3b3ed727c985f
Simran <51385869+simran1802@users.noreply.github.com>
2020-10-04 15:48:13 +0530

    Remove Covered Intervals

---
+++ 4.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        int res=0;
+        int left = -1, right = -1;
+        sort(intervals.begin(),intervals.end());
+        for(auto &i : intervals){
+            if(i[0] > left && i[1] > right){
+                left = i[0];
+                ++res;
+            }
+            right = max(right,i[1]);
+        }
+        return res;
+    }
+};
